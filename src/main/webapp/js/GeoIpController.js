f3b4e6c85c11b0ffe3512c4c7fb2fb699d9334b8
maki <maki@bitkings.de>
2012-11-23 00:06:26 +0100

    added marker for country

--- src/main/webapp/js/GeoIpController.js
+++ src/main/webapp/js/GeoIpController.js
@@ -1,10 +1,12 @@
-
 function GeoIpController($scope, $http) {
-    $scope.country = {name : "n.a.", code: "n.a.", remoteHost: "n.a."};
+   $scope.country = {name:"n.a.", code:"n.a.", remoteHost:"n.a."};
 
-    $scope.getCountry = function() {
-        $http.get('/rest/resolve').success(function (data) {
-            $scope.country = data;
-        });
-    }
+   $scope.getCountry = function () {
+      $http.get('/rest/resolve').success(function (data) {
+         $scope.country = data;
+         var values = {};
+         values[data.code] = 1;
+         _worldmap.series.regions[0].setValues(values);
+      });
+   }
 }
